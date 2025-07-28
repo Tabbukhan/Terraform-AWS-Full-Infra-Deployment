36605db06ef51308e600c08bcf1c4ddbd1ca0c39
Tabasum khan <tabasumkhanmca@gmail.com>
2025-07-28 13:27:35 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,8 +4,9 @@ resource "random_id" "suffix" {
 
 resource "aws_s3_bucket" "this" {
   bucket = "${var.bucket_name}-${random_id.suffix.hex}"
-
+  force_destroy = true
   tags = {
     Name = "${var.bucket_name}-${random_id.suffix.hex}"
   }
+  provider = aws.ap_south_1  # <-- Use named provider
 }
