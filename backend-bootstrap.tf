0b7b84f89619393edc370e52c809993c2f26c358
Tabasum khan <tabasumkhanmca@gmail.com>
2025-07-29 17:22:08 +0530

    Create backend-bootstrap.tf

---
+++ backend-bootstrap.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-backend-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "dev"
+  }
+}
