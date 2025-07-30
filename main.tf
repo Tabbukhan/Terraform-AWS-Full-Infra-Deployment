1021f13f2c56573a955bba3dff65217256621599
Tabasum khan <tabasumkhanmca@gmail.com>
2025-07-30 11:39:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-backend-bucket"
-    key            = "terraform.tfstate"
+    key            = "env:/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
