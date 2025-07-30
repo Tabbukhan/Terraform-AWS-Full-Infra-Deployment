18f839d449709320dfb0105db1d52b75264d802a
Tabasum khan <tabasumkhanmca@gmail.com>
2025-07-30 11:35:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@
 #   alias  = "ap_south_1"
 #}
 
-#terraform {
-#  backend "s3" {
- #   bucket         = "my-terraform-backend-bucket"
-  #  key            = "terraform.tfstate"
-    #region         = "ap-south-1"
-    #dynamodb_table = "terraform-locks"
-   # encrypt        = true
-  #}
-#}
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-backend-bucket"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 
 
 # S3 Module
